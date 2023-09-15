ef056ceadddc03f3c7917a7b430c273227977f60
J.W. Tan <55975997+jwtanx@users.noreply.github.com>
2023-09-15 10:00:40 +0800

    230915 - Update setup.py to install contrib package

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with codecs.open('README.rst', 'r', 'utf-8') as fd:
       author='Google Inc.',
       maintainer='Bill Wendling',
       maintainer_email='morbo@google.com',
-      packages=['yapf', 'yapf.yapflib', 'yapftests'],
+      packages=['contrib', 'yapf', 'yapf.yapflib', 'yapftests'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
